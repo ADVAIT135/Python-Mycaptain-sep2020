942d613f5ed2d96263223820b8ce5ac91d4d3414
ADVAIT135 <59262727+ADVAIT135@users.noreply.github.com>
2020-09-17 21:51:30 +0530

    Program to find positive numbers from a list
    This program take a list of numbers from user and then prints the positive number from that list

---
+++ Task 4 program to find all the positive numbers given in a list.py
@@ -0,0 +1,10 @@
+NumList = []
+Num=int(input("Please enter the total number of elements present in the list:"))
+for i in range(0,Num):
+    val=int(input("Please enter the value of the %d Element: "%i))
+    NumList.append(val)
+
+print("Positive Numbers in the list are : ")
+for j in range (Num):
+    if(NumList[j] >= 0):
+        print(NumList[j],end=' ')
